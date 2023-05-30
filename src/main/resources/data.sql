98a84298b17ed9be96cb0da31dc006c1ac70ad95
“your <londal.anette@gmail.com>
2023-05-30 10:15:48 +0200

    added sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO article (article_id , title , body , published_on , author)
+VALUES
+('1','The first article','This is the first article of this program', '2023.30.05','Mr AA'),
+('2','Test nr 2!','The article is ALIVE', '2023.22.05','Bob'),
+('3','Another Test','bla bla bal', '2021.22.05','Mr AA');
