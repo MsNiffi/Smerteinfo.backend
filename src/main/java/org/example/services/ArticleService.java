ca832c558438ef4711e13fafaff71576b8b0ceb3
“your <londal.anette@gmail.com>
2023-05-30 03:39:45 +0200

    added findByAuthor method to serbice layer.

--- src/main/java/org/example/services/ArticleService.java
+++ src/main/java/org/example/services/ArticleService.java
@@ -1,7 +1,10 @@
 package org.example.services;
 
+import org.example.models.enteties.Article;
 import org.example.repositories.ArticleRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ArticleService {
@@ -12,7 +15,10 @@ private final ArticleRepository articleRepository;
         this.articleRepository = articleRepository;
     }
 
+public List<Article> findAllByAuthor (String author){
+        return articleRepository.findAllByAuthor(author);
 
+}
 
 
 
