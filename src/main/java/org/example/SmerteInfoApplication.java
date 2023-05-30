d05c4dd8a63d673dd0bbbf24ce2e3dc192d82e0e
“your <londal.anette@gmail.com>
2023-05-30 10:50:49 +0200

    fixed bug in data.sql now it runs

--- src/main/java/org/example/SmerteInfoApplication.java
+++ src/main/java/org/example/SmerteInfoApplication.java
@@ -1,18 +1,18 @@
 package org.example;
 
-import org.example.services.ArticleService;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.example.services.ArticleService;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmerteInfoApplication {
 
-private final ArticleService articleService;
+    private final ArticleService articleService;
 
 
-public SmerteInfoApplication (ArticleService articleService){
-    this.articleService = articleService;
-}
+    public SmerteInfoApplication (ArticleService articleService){
+        this.articleService = articleService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SmerteInfoApplication.class, args);
